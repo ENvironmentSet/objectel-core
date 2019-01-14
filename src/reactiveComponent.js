f808cf3c8e4053a215ce10c9277d2c1979d0c55c
ENvironmentSet <sbshw1@gmail.com>
2019-01-14 16:22:41 +0900

    remove old reactiveComponentWithProps

--- src/reactiveComponent.js
+++ src/reactiveComponent.js
@@ -1,18 +1,7 @@
 import { compose } from './utils';
 
-function reactiveComponentWithFactory(componentFactory) {
-  return componentFactory;
-}
-
-function reactiveComponentWithoutFactory(intent$, model$, result$) {
+export default function reactiveComponent(intent$, model$, result$) {
   const component = compose(result$, model$, intent$);
 
   return () => component;
-}
-
-export default function reactiveComponent(...args) {
-  return args.length === 1 ?
-    reactiveComponentWithFactory(...args)
-    :
-    reactiveComponentWithoutFactory(...args);
 }
